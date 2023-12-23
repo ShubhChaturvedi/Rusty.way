626679498f84d9ca58de77c162308b39ed9752b3
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2023-12-23 20:27:53 +0530

    FINISH : Chapter 02: guessing game

--- guessingGame/src/main.rs
+++ guessingGame/src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
-    println!("Hello, world!");
+    println!("welcome to the guessing game");
+
+    let secret_number  : u32 = rand::thread_rng().gen_range(1, 101);
+
+    loop{
+        println!("please input your guess");
+
+        let mut guess: String = String::new();
+
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read number");
+
+        println!("you guessed: {}", guess);
+
+        match guess.trim().parse::<u32>().expect("not a number").cmp(&secret_number){
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            },
+        }
+    }
+
 }
